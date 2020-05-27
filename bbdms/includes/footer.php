e555a19c2e257cde80c300016e415d362a7ccad5
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2020-05-27 21:41:32 +0530

    Add files via upload

---
+++ bbdms/includes/footer.php
@@ -0,0 +1,8 @@
+
+
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+        <pre>    <p class="m-0 text-center text-white">&copy BloodBank & Donor Management System <br>Aditya Kumar IIIT MANIPUR</p>   </pre>
+        </div>
+        <!-- /.container -->
+    </footer>
